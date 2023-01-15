b2dc352757dfef29ade1f6f673b5f1a5400306b5
EbenGitHub <abenezergoo@gmail.com>
2023-01-16 02:26:56 +0300

    up

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,18 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="eben12ysh/app"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+# Tag the application with version name
+docker tag app ${dockerpath}:v1.0.0
+
+# Login to docker account
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
 # Step 3:
 # Push image to a docker repository
+docker push ${dockerpath}:v1.0.0

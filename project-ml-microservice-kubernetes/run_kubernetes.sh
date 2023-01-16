012adb95d35a7e5dca3ceec5e7794303915dd91d
EbenGitHub <abenezergoo@gmail.com>
2023-01-16 03:22:19 +0300

    kube

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="eben12ysh/app"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run appdemo \
+    --image=${dockerpath}:v1.0.0\
+    --port=80 --labels app=appdemo
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pods/appdemo 8000:80
